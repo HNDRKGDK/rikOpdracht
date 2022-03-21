464b816c30ffa810103a47741a002a06f7db71ed
HNDRKGDK <84562602+HNDRKGDK@users.noreply.github.com>
2022-03-21 10:27:21 +0100

    add console project

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace rikOpdracht
+{
+    class Program
+    {
+        static void sayHello()
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static void Main(string[] args)
+        {
+            sayHello();
+        }
+    }
+}
